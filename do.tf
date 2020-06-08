b1f53258bcda0a8c6acbce7c4f27029fc084560e
freemanpd <patrick.freeman@sftech.io>
2020-06-07 22:34:43 -0400

    commiting

--- do.tf
+++ do.tf
@@ -1,10 +1,10 @@
 module "hadooplab" {
   source             = "./modules"
-  droplet_name              = "hadooplab-sa"
+  droplet_name       = "hadooplab-sa"
   region             = "nyc1"
-  image_name            = "centos-7-x64"
+  image_name         = "centos-7-x64"
   droplet_count      = "1"
- do_slug     = "s-6vcpu-16gb"
+  do_slug            = "s-6vcpu-16gb"
   disk_size          = "100"
   private_networking = "false"
   backups            = "false"
